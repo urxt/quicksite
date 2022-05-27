f9d383545711d72c1a48cd63a20e885c9cee6fbe
urxt <80124020+urxt@users.noreply.github.com>
2022-05-26 23:27:21 -0400

    new test.py added

--- test.py
+++ test.py
@@ -1,9 +1,1 @@
 #!/usr/bin/env python3
-def main():
-    with open('templates/earth/template.html', 'r') as f:
-        myfile = f.read().replace("{ List of Blogpages }", "Stuff")
-
-    print(myfile)
-
-if __name__ == "__main__":
-    main()
