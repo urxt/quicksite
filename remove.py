97d8fc930020834ba9a25049a6e9722d9e069db4
urxt <80124020+urxt@users.noreply.github.com>
2022-05-28 00:20:01 -0400

    modified remove script

--- remove.py
+++ remove.py
@@ -4,15 +4,27 @@ from os import walk, remove
 def remove_blog():
     path = "./blogpages/"
     filenames = next(walk(path), (None, None, []))[2]
-    for index, f in enumerate(filenames):
+    for index, f in enumerate(filenames):   
         print("{}: {}".format(index, f))
 
-    file = input("Delete which file?: ")
-    remove("blogpages/{}.md".format(file))
+    file = input("Delete which file?: ").strip()
+    if file in filenames:
+        remove("blogpages/{}".format(file))
+        with open("bloglist.md", "+a") as f:
+            f.seek(0)
+            if file in f.read()
+                f.truncate()
+                
+
+        print("File: {} deleted".format(file))
+
+    else:
+        print("Invalid filename")
+
 
 
 def main():
-    list_items()
+    remove_blog()
 
 if __name__ == "__main__":
     main()
